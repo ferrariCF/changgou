b4d9eea1579a2e98767cd9f91a2ae774790e8c26
Lee <273645074@qq.com>
2021-07-30 11:50:14 +0800

    项目初始化

---
+++ changgou-parent/changgou-eureka/src/main/java/com/changgou/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.changgou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: lee
+ * @date: 2021-07-30
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
@@ -0,0 +1,17 @@
+package com.changgou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author: lee
+ * @date: 2021-07-30
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class GoodsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsApplication.class,args);
+    }
+}
