356591d9572a4a6b6cf568108608d2c10e60c6e1
Lee <273645074@qq.com>
2021-08-04 23:10:57 +0800

    完善搜索功能，通过设置es分组条件获取搜索结果的分类列表和品牌列表

--- changgou-parent/changgou-service/changgou-service-search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
+++ changgou-parent/changgou-service/changgou-service-search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
@@ -9,6 +9,8 @@ import com.changgou.search.service.SkuService;
 import entity.Result;
 import org.apache.commons.lang.StringUtils;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
@@ -16,6 +18,7 @@ import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilde
 import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,8 +37,11 @@ public class SkuServiceImpl implements SkuService {
     private SkuEsMapper skuEsMapper;
 
     @Autowired
-    private ElasticsearchTemplate elasticsearchTemplate;
+    private ElasticsearchTemplate esTemplate;
 
+    /**
+     * 将数据库中的数据查询出来导入 es
+     */
     @Override
     public void importSku() {
         // 1. 使用 feign 调用商品微服务，获取商品数据
@@ -66,24 +72,51 @@ public class SkuServiceImpl implements SkuService {
         }
         // 2. 创建查询对象的构建对象
         NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+        // 2.1 设置根据分类的分组查询条件
+        nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms("skuCategoryGroup").field("categoryName").size(100));
+        // 2.2 设置根据品牌的分组查询条件
+        nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms("skuBrandGroup").field("brandName").size(100));
+
         // 3. 设置条件 匹配查询
         nativeSearchQueryBuilder.withQuery(QueryBuilders.matchQuery("name",keywords));
         // 4. 构建查询对象
         SearchQuery query = nativeSearchQueryBuilder.build();
         // 5. 执行查询
-        AggregatedPage<SkuInfo> skuInfos = elasticsearchTemplate.queryForPage(query, SkuInfo.class);
+        AggregatedPage<SkuInfo> skuPage = esTemplate.queryForPage(query, SkuInfo.class);
         // 6. 获取结果 封装返回
         // 6.1 当前页的记录
-        List<SkuInfo> content = skuInfos.getContent();
+        List<SkuInfo> content = skuPage.getContent();
         // 6.2 根据条件命中的总记录数
-        long totalElements = skuInfos.getTotalElements();
+        long totalElements = skuPage.getTotalElements();
         // 6.3 总页数
-        int totalPages = skuInfos.getTotalPages();
+        int totalPages = skuPage.getTotalPages();
+        // 6.4 分组结果(分类列表)
+        List<String> categoryList = getGroupNameList(skuPage, "skuCategoryGroup");
+        // 6.5 分组结果(品牌列表)
+        List<String> brandList = getGroupNameList(skuPage, "skuBrandGroup");
+
         // 封装到 Map 中
         Map<String,Object> resultMap = new HashMap<>();
         resultMap.put("rows",content);
         resultMap.put("total",totalElements);
         resultMap.put("totalPages",totalPages);
+        resultMap.put("categoryList",categoryList);
+        resultMap.put("brandList",brandList);
         return resultMap;
+    }
+
+    /**
+     * 获取组名的列表
+     * @param skuPage 查询结果
+     * @param skuGroup 分组查询的标识名
+     * @return
+     */
+    private List<String> getGroupNameList(AggregatedPage<SkuInfo> skuPage, String skuGroup) {
+        List<String> groupNameList = new ArrayList<>();
+        StringTerms stringTerms = (StringTerms) skuPage.getAggregation(skuGroup);
+        if (stringTerms != null) {
+            stringTerms.getBuckets().forEach(bucket -> groupNameList.add(bucket.getKeyAsString()));
+        }
+        return groupNameList;
     }
 }
