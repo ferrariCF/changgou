1ca8e87ebaea85640f7d20de05237a263e4e54d8
Lee <273645074@qq.com>
2021-08-07 16:58:13 +0800

    完善搜索功能，过滤查询(分类、品牌、规格、价格范围)，分页排序

--- changgou-parent/changgou-service/changgou-service-search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
+++ changgou-parent/changgou-service/changgou-service-search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
@@ -8,10 +8,15 @@ import com.changgou.search.pojo.SkuInfo;
 import com.changgou.search.service.SkuService;
 import entity.Result;
 import org.apache.commons.lang.StringUtils;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
@@ -58,7 +63,9 @@ public class SkuServiceImpl implements SkuService {
     /**
      * 暂时只做关键字搜索
      *
-     * @param searchMap {"keywords":"手机"}
+     * @param searchMap {"keywords":"华为","category":"你点击的分类的值","brand":"你点击到的品牌的值",
+     *                  "spec_网络制式":"电信2G","price":"0-500","sortField":"price","sortRule":"DESC",
+     *                  "pageNum":"1"}
      * @return
      */
     @Override
@@ -80,6 +87,62 @@ public class SkuServiceImpl implements SkuService {
 
         // 3. 设置条件 匹配查询
         nativeSearchQueryBuilder.withQuery(QueryBuilders.matchQuery("name", keywords));
+        // 3.1 过滤查询
+        // 多条件组合查询
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+
+        // 商品分类过滤
+        String category = searchMap.get("category");
+        if (!StringUtils.isEmpty(category)) {
+            boolQueryBuilder.filter(QueryBuilders.termQuery("categoryName",category));
+        }
+        // 商品品牌过滤
+        String brand = searchMap.get("brand");
+        if (!StringUtils.isEmpty(brand)) {
+            boolQueryBuilder.filter(QueryBuilders.termQuery("brandName",brand));
+        }
+        // 商品规格过滤
+        for (Map.Entry<String, String> entry : searchMap.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (key.startsWith("spec_")) {
+                boolQueryBuilder.filter(QueryBuilders.termQuery("specMap." + key.substring(5) + ".keyword",value));
+            }
+        }
+        // 价格区间过滤(范围)
+        String price = searchMap.get("price");
+        if (!StringUtils.isEmpty(price)) {
+            String[] split = price.split("-");
+            if ("*".equals(split[1])){
+                // 3000元以上-->3000-*
+                boolQueryBuilder.filter(QueryBuilders.rangeQuery("price").gte(split[0]));
+            }else{
+                boolQueryBuilder.filter(QueryBuilders.rangeQuery("price").gte(split[0]).lte(split[1]));
+            }
+        }
+        // 过滤
+        nativeSearchQueryBuilder.withFilter(boolQueryBuilder);
+
+        // 3.2 分页查询，设置分页对象，指定当前页码和每页条数
+        String pageNumString = searchMap.get("pageNum");
+        Integer pageNum = 1;
+        if (!StringUtils.isEmpty(pageNumString)) {
+            pageNum = Integer.valueOf(pageNumString);
+        }
+        Integer size = 20;
+        // 设置分页对象
+        Pageable pageable = PageRequest.of(pageNum - 1,size);
+        nativeSearchQueryBuilder.withPageable(pageable);
+
+        // 3.3 设置排序
+        // 排序字段
+        String sortField = searchMap.get("sortField");
+        // 排序规则
+        String sortRule = searchMap.get("sortRule");
+        if (!StringUtils.isEmpty(sortField) && !StringUtils.isEmpty(sortRule)){
+            nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort(sortField).order(SortOrder.valueOf(sortRule)));
+        }
+
         // 4. 构建查询对象
         SearchQuery query = nativeSearchQueryBuilder.build();
         // 5. 执行查询
